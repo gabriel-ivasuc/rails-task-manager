74ad805864f5dc7e15afce4891d215b54677afa4
Gabriel Ivasuc <gabrielivasuc@yahoo.com>
2024-04-30 16:49:33 +0100

    displayed details of a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,17 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:restaurant])
+    @task.save
+  end
 end
