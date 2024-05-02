c46cca61ed829b86d0ecbf7ca71a7e06a1aaf29f
Gabriel Ivasuc <gabrielivasuc@yahoo.com>
2024-05-02 21:02:26 +0100

    completed CRUD operations, need to add some style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to show_path(@task)
+    redirect_to task_path(@task)
   end
 
   def edit
@@ -25,7 +25,13 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to show_path(@task)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path, status: :see_other
   end
 
   private
