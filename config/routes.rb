b0db3873993ef7634841de99d615e5405a21f572
Gabriel Ivasuc <gabrielivasuc@yahoo.com>
2024-05-02 10:33:18 +0100

    rails task manager to be completed

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get '/tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new' , as: 'new_task'
+
   get '/tasks/:id', to: 'tasks#show', as: 'show'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
 end
